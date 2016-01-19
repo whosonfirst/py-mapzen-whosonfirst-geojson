76b779e2342220636dce02c37797611927a427f9
Dan Phiffer <dan@phiffer.org>
2016-01-19 15:43:01 -0800

    🔧 Added VERSION to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.geojson.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+version = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.geojson',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.geojson'],
-    version='0.06',
+    version=version,
     description='Python tools for doing GeoJSON related things with Who\'s On First data',
     author='Mapzen',
-    url='https://github.com/mapzen/py-mapzen-whosonfirst-geojson',
+    url='https://github.com/whosonfirst/py-mapzen-whosonfirst-geojson',
     install_requires=[
         ],
     dependency_links=[
@@ -20,5 +32,5 @@ setup(
     scripts=[
         'scripts/wof-encode',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-geojson/releases/tag/v0.06',
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-geojson/releases/tag/' + version,
     license='BSD')
