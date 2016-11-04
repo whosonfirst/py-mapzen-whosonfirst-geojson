acf82aed050ebad325e49a757e9f45f06bc64727
thisisaaroland <aaron@mapzen.com>
2016-11-04 16:13:16 -0700

    namespace wrangling for pip; bump to 0.07

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ version = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.geojson',
-    namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.geojson'],
+    namespace_packages=['mapzen', 'mapzen.whosonfirst'],
     version=version,
     description='Python tools for doing GeoJSON related things with Who\'s On First data',
     author='Mapzen',
